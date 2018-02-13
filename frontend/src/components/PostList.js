cd0e20320c06b1f9b19cd737f36e9b525dc938bb
droidstick <droidstick@gmail.com>
2018-02-13 13:46:28 +0700

    feat: Show PostList component at the root

---
+++ frontend/src/components/PostList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PostList extends Component {
+    render() {
+        return (
+            <h1>Post list</h1>
+        );
+    }
+}
+
+export default PostList;
