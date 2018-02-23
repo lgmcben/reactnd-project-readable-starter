cc1ede7a8c3b6616144c9a39cf5aa4e40daa44b4
droidstick <droidstick@gmail.com>
2018-02-23 14:37:18 +0700

    feat: Successfully submit dummy post

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,11 @@
 function fetchPostList () {
     const postListUrl = `http://localhost:3001/posts`;
     return fetch(postListUrl, {
-               method: 'GET',
-               headers: { 'Authorization': 'benkitti2' }
+                method: 'GET',
+                headers: {
+                    'Authorization': 'kittiben',
+                    'Content-Type': 'application/json'
+                },
            })
            .then((response) => response.json())
            .then((postList) => postList);
@@ -10,10 +13,22 @@ function fetchPostList () {
 
 function submitNewPost () {
     const uuidv1 = require('uuid/v1');
-    const url = `http://localhost:3001/posts?id=${uuidv1()}&timestamp=${Date.now()}&title=testTitle&body=testBody&author=ben&category="udacity"`;
+    const url = 'http://localhost:3001/posts';
+    let postBody = JSON.stringify({
+       id: uuidv1(),
+       timestamp: Date.now(),
+       title: 'testTitle',
+       body: 'testBody',
+       author: 'ben',
+       category: 'udacity'
+     })
     return fetch(url, {
-               method: 'POST',
-               headers: { 'Authorization': 'benkitti2' }
+                method: 'POST',
+                headers: {
+                    'Authorization': 'kittiben',
+                    'Content-Type': 'application/json'
+                },
+                body: postBody
            })
            .then((response) => {
                 console.log(response)
