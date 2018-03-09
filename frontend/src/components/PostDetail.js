6cec52d392b4b3e9e1402177bbfc48d2b8f9e3b3
droidstick <droidstick@gmail.com>
2018-03-09 14:45:43 +0700

    feat: Show 404 not found when user try to open post with invalid id (eg. deleted)

--- frontend/src/components/PostDetail.js
+++ frontend/src/components/PostDetail.js
@@ -5,35 +5,40 @@ import {  upVoteRequest, downVoteRequest } from '../actions'
 class PostDetail extends Component{
     render() {
         const post = this.props.postList.find(post => post.id === this.props.match.params.post_id);
-        return(
-            <div>
-                <p>
-                    <strong>{post.title}</strong>
-                </p>
-                <p>
-                    {post.body}
-                </p>
-                <p>
-                    <i>author: {post.author} | comments: {post.commentCount} | score: {post.voteScore}</i>
-                </p>
+        if(post){
+            return(
+                <div>
+                    <p>
+                        <strong>{post.title}</strong>
+                    </p>
+                    <p>
+                        {post.body}
+                    </p>
+                    <p>
+                        <i>author: {post.author} | comments: {post.commentCount} | score: {post.voteScore}</i>
+                    </p>
 
-                <button className='button-control' onClick={() => this.props.dispatchUpvote(post.id)}>
-                    + Upvote
-                </button>
+                    <button className='button-control' onClick={() => this.props.dispatchUpvote(post.id)}>
+                        + Upvote
+                    </button>
 
-                <button className='button-control' onClick={() => this.props.dispatchDownvote(post.id)}>
-                    - Downvote
-                </button>
+                    <button className='button-control' onClick={() => this.props.dispatchDownvote(post.id)}>
+                        - Downvote
+                    </button>
 
-                <button className='button-control'>
-                    Edit
-                </button>
+                    <button className='button-control'>
+                        Edit
+                    </button>
 
-                <button className='button-control'>
-                    Delete
-                </button>
-            </div>
-        )
+                    <button className='button-control'>
+                        Delete
+                    </button>
+                </div>
+            )
+        } else {
+            return <h2>404 page not found</h2>
+        }
+
     }
 }
 
