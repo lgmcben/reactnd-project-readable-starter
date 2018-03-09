4adfbb540460f99c307ac8e6ba3745e863e6e47f
droidstick <droidstick@gmail.com>
2018-03-09 12:37:47 +0700

    feat: Display post_id in detail page

--- frontend/src/components/PostDetail.js
+++ frontend/src/components/PostDetail.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class PostDetail extends Component{
     render() {
         return(
-            <h1>Post detail </h1>
+            <h1>{this.props.match.params.post_id} </h1>
         )
     }
 }
 
-export default PostDetail;
+function mapStateToProps ({post, comment, category}, ownProps) {
+    return {
+        postList: post.postList,
+    }
+}
+
+function mapDispatchToProps (dispatch) {
+    return {
+
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostDetail);
