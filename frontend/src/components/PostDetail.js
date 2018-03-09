ba2bce48285e104fc248dee1020933029ed96ea2
droidstick <droidstick@gmail.com>
2018-03-09 14:20:46 +0700

    feat: Show all elements in post detail page

--- frontend/src/components/PostDetail.js
+++ frontend/src/components/PostDetail.js
@@ -5,8 +5,33 @@ class PostDetail extends Component{
     render() {
         const post = this.props.postList.find(post => post.id === this.props.match.params.post_id);
         return(
-            <h1>{post.title}</h1>
+            <div>
+                <p>
+                    <strong>{post.title}</strong>
+                </p>
+                <p>
+                    {post.body}
+                </p>
+                <p>
+                    <i>author: {post.author} | comments: {post.commentCount} | score: {post.voteScore}</i>
+                </p>
 
+                <button className='button-control' onClick={() => this.props.dispatchUpvote(post.id)}>
+                    + Upvote
+                </button>
+
+                <button className='button-control' onClick={() => this.props.dispatchDownvote(post.id)}>
+                    - Downvote
+                </button>
+
+                <button className='button-control'>
+                    Edit
+                </button>
+
+                <button className='button-control'>
+                    Delete
+                </button>
+            </div>
         )
     }
 }
