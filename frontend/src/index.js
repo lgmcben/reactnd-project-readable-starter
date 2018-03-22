f6826c15a198280d690a4c40862882979a84a33d
BEER-DELL\lgmcb <droidstick@gmail.com>
2018-03-22 16:29:45 +0700

    chore: Clean up src/index.js and App.css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,29 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware, compose } from 'redux'
-import reducer from './reducers'
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
-const logger = store => next => action => {
-   console.group(action.type)
-   console.info('dispatching', action)
-   let result = next(action)
-   console.log('next state', store.getState())
-   console.groupEnd(action.type)
-   return result
-}
+import './index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+import reducer from './reducers'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
     reducer,
     composeEnhancers(
-        applyMiddleware(thunk, logger)
+        applyMiddleware(thunk)
     )
 )
 
